6e4f4bc56e9b4193d9666fcb57db08422eb2a1cf
Rohit Nimkar <nehalnimkar@gmail.com>
2024-01-07 12:49:04 +0530

    ♻️ refactor

--- blink/include/delay.h
+++
@@ -1,7 +0,0 @@
-#include <stm32f1xx.h>
-
-#pragma once
-
-void ms_delay(int ms);
-
-void unused_delay(int us);
--- blink/src/main.c
+++ blink/src/main.c
@@ -1,22 +1,49 @@
 /**
- ******************************************************************************
- * @file           : main.c
- * @author         : Rohit Nimkar <nehalnimkar@gmail.com> <https://csrohit.github.io>
- * @brief          : Main program body
- ******************************************************************************
+ * @file     main.c
+ * @author   Rohit Nimkar <https://www.linkedin.com/in/rohit-nimkar>
+ * @brief    Blink LED connected to PC13
+ * @version  1.2
+ * @date     2024-01-06
+ *
+ * @copyright Copyright (c) 2024
+ *
+ * @attention
+ * This software component is licensed by Rohit Nimkar under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
+ *
  */
 
 #include <stm32f1xx.h>
-#include <delay.h>
 
-
+void ms_delay(int ms)
+{
+    while (ms-- > 0)
+    {
+        volatile int x = 1000;
+        while (x-- > 0)
+            __asm("nop");
+    }
+}
 
 int main(void)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	GPIOC->CRH = 0x02 << ((13 - 8) << 2);
-	while(1){
-		GPIOC->ODR ^= 1 << 13;
-		ms_delay(1000);
-	}
+    /* Enable clock for port C */
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    /* Set PC13 as General Purpose Output @2MHZ */
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+    GPIOC->CRH |= GPIO_CRH_MODE13_1;
+
+    while (1)
+    {
+        /* Set bit corresponding to PC13 */
+        GPIOC->BSRR = GPIO_BSRR_BS13;
+        ms_delay(1000);
+
+        /* Reset bit corresponding to PC13 */
+        GPIOC->BRR = GPIO_BRR_BR13;
+        ms_delay(1000);
+    }
 }
+
